c6a0fe40f08f65f26b8991197f536bb352f2924b
Honest91 <mario.mokhles@gmail.com>
2023-03-21 15:29:08 -0700

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,15 @@
+#include"main.h"
+
+/**
+ * print_alphabet_x10 - function that will print the alphabet 10 time
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+	int line, ch;
+
+	for (line = 0; line <= 9; line++)
+		_putchar(ch);
+	_putchar('\n');
+}
